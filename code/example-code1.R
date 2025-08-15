5aedf5031ccf85725101e8b1eb200a092db3027a
katiewampler <katie.wampler@oregonstate.edu>
2025-08-15 12:15:33 -0700

    updating code1 example to focus on file paths

--- code/example-code1.R
+++ code/example-code1.R
@@ -1,7 +1,10 @@
-# example code file for learning how to use Git and GitHub 
+# example code file for learning how to use Git and GitHub
 
-#read and edit .csv 
-  #df <- read.csv("C:/Users/wampleka/Documents/WWS-Node1-TEST-example-repo/data/example-csv.csv")
-  df <- read.csv("data/example-csv.csv")
+username <- "smith"
+file <- file.path("C:/Users", username, "Documents/WWS-Node1-TEST-example-repo/data/example-csv.csv")
+print(file)
+
+#read and edit .csv
+  df <- read.csv(file)
   df$value <- sample(1:100, size=4)
-  write.csv(df, "data/example-csv.csv")  
+  write.csv(df, file)
