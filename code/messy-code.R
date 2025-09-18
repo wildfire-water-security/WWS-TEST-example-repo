35b2a4d2efcf94eba8be0ee0386a0a8e282fb988
katiewampler <katie.wampler@oregonstate.edu>
2025-09-18 15:30:46 -0700

    adding messy example code

---
+++ code/messy-code.R
@@ -0,0 +1,22 @@
+# Messy example R script
+
+library(dplyr)
+library(ggplot2)
+
+data <- read.csv("C:/Users/Katie/Documents/WWS/Data/example-csv.csv")
+
+data$value2 <- data$value - 2
+data$value3 <- data$value - 4
+data$value3 <- data$value - 10
+
+ggplot(data, aes(x = value, y = value2)) +
+  geom_line() + ggtitle("value minus a constant")
+ggsave("C:/Users/Katie/Documents/WWS/Plots/value_minus_2.png")
+
+ggplot(data, aes(x = value, y = value3)) +
+  geom_line() + ggtitle("value minus a constant")
+ggsave("C:/Users/Katie/Documents/WWS/Plots/value_minus_4.png")
+
+ggplot(data, aes(x = value, y = value3)) +
+  geom_line() + ggtitle("value minus a constant")
+ggsave("C:/Users/Katie/Documents/WWS/Plots/value_minus_10.png")
