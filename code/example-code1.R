7407975fc2d3603c3dc39d398b52c16fa2993e03
Murshed <murshedh@oregonstate.edu>
2026-01-28 13:42:00 -0800

    I have changed and added my username to the example code
    1. we could add bullet points

--- code/example-code1.R
+++ code/example-code1.R
@@ -1,6 +1,6 @@
 # example code file for learning how to use Git and GitHub
 
-username <- "PaigeScottie"
+username <- "murshedh"
 file <- file.path("C:/Users", username, "Documents/WWS-TEST-example-repo/data/example-csv.csv")
 print(file)
 
