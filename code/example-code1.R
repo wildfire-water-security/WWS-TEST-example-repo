7cf181cc93ed0de15b4cab5767c893688d3d8192
KateMcCredie <kate.mccredie@oregonstate.edu>
2026-01-28 13:43:04 -0800

    GitHub Workshop
    -created R project
    -changing file path to be more collaborative
    -ignoring nuisance files from R, Office, Windows, and Mac
    
    Co-Authored-By: kappenty <220963970+kappenty@users.noreply.github.com>

--- code/example-code1.R
+++ code/example-code1.R
@@ -1,7 +1,7 @@
 # example code file for learning how to use Git and GitHub
 
-username <- "PaigeScottie"
-file <- file.path("C:/Users", username, "Documents/WWS-TEST-example-repo/data/example-csv.csv")
+username <- "mccredik"
+file <- file.path("data/example-csv.csv")
 print(file)
 
 #read and edit .csv
