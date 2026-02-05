cd2b9d5669c0f93035c9253afae35f0555239d1d
rileybbarton <rileybbarton@gmail.com>
2026-02-05 09:02:25 -0800

    test examples
    Riley-Humyra team going through module

--- code/example-code2.R
+++ code/example-code2.R
@@ -1,9 +1,9 @@
-# example code file for learning how to use Git and GitHub 
+# example code file for learning how to use Git and GitHub
 
-#make a dummy figure 
+#make a dummy figure
   library(ggplot2)
   data <- read.csv("data/example-csv.csv")
+  data$X <- c(rep("Group 1", 4), rep("Group 2", 4))
   ggplot(data, aes(x=site, y=value)) + geom_point() + geom_line()
-  
+
   ggsave("figures/figure1.png")
-  
