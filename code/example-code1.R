a61f73bb76063e61eada0e8ec7c0898f59568cf8
enussdorfer <nussdore@oregonstate.edu>
2026-03-18 15:20:05 -0700

    Github workshop
        creating R project
        changing file path to relative to be be more collaborative
        ignoring nuisance files from R, Office, Windows, and Mac

--- code/example-code1.R
+++ code/example-code1.R
@@ -1,10 +1,14 @@
 # example code file for learning how to use Git and GitHub
 
-username <- "smith"
-file <- file.path("C:/Users", username, "Documents/WWS-TEST-example-repo/data/example-csv.csv")
+username <- "nussdore"
+file <- file.path("C:/Users", username, "Documents/Github/WWS-TEST-example-repo/data/example-csv.csv")
 print(file)
 
 #read and edit .csv
   df <- read.csv(file)
   df$value <- sample(1:100, size=4)
   write.csv(df, file, row.names=FALSE)
+  library(usethis)
+  repo <- file.path(fs::path_home(), "Documents/Github/WWS-TEST-example-repo/")
+  usethis::create_project(repo)
+g  
