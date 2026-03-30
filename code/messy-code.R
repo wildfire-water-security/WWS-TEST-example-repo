997fafb0bc2ec2ae8a390637fd615fac4d6f0dc3
rileybbarton <rileybbarton@gmail.com>
2026-03-30 10:02:14 -0700

    last module

--- code/messy-code.R
+++ code/messy-code.R
@@ -1,22 +1,26 @@
 # Messy example R script
 
+#load in libraries
 library(dplyr)
 library(ggplot2)
 
-data <- read.csv("C:/Users/Katie/Documents/WWS/Data/example-csv.csv")
+#read in data
+data <- read.csv("data/example-csv.csv")
 
+#subtract value by constant
 data$value2 <- data$value - 2
 data$value3 <- data$value - 4
 data$value3 <- data$value - 10
 
+#plot and save
 ggplot(data, aes(x = value, y = value2)) +
   geom_line() + ggtitle("value minus a constant")
-ggsave("C:/Users/Katie/Documents/WWS/figures/value_minus_2.png")
+ggsave("figures/value_minus_2.png")
 
 ggplot(data, aes(x = value, y = value3)) +
   geom_line() + ggtitle("value minus a constant")
-ggsave("C:/Users/Katie/Documents/WWS/figures/value_minus_4.png")
+ggsave("figures/value_minus_4.png")
 
 ggplot(data, aes(x = value, y = value3)) +
   geom_line() + ggtitle("value minus a constant")
-ggsave("C:/Users/Katie/Documents/WWS/figures/value_minus_10.png")
+ggsave("figures/value_minus_10.png")
